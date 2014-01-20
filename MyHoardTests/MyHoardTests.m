bfc6fb5e07450fa58ccdb4caf0f8d5a068464163
Karol Kogut <karol.kogut@blstream.com>
2014-01-20 16:31:11 +0100

    + Added sample Kiwi's tests. - Removed OCMock.

--- MyHoardTests/MyHoardTests.m
+++ MyHoardTests/MyHoardTests.m
@@ -6,29 +6,77 @@
 //  Copyright (c) 2013 BLStream. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+/*
+ * More about Kiwi syntax:
+ * https://github.com/allending/Kiwi/wiki/Specs
+ * https://github.com/allending/Kiwi/wiki/Expectations
+ * https://github.com/allending/Kiwi/wiki/Mocks-and-Stubs
+ */
 
-@interface MyHoardTests : XCTestCase
+#import "Kiwi.h"
 
-@end
+SPEC_BEGIN(MathSpec)
 
-@implementation MyHoardTests
+describe(@"Sample tests", ^{
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+    context(@"Test if numbers are equal", ^{
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+        it(@"Numbers should be equal", ^{
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
+            NSUInteger a = 16;
+            NSUInteger b = 26;
+            [[theValue(a + b) should] equal:theValue(42)];
+        });
 
-@end
+        it(@"Numbers should not be equal", ^{
+
+            NSUInteger a = 10;
+            NSUInteger b = 5;
+            [[theValue(a - b) shouldNot] equal:theValue(15)];
+        });
+    });
+
+    context(@"Test strings", ^{
+
+        it(@"Chcek if strings are equal", ^{
+
+            NSString* s1 = @"test";
+            NSString* s2 = @"test";
+            NSString* s3 = @"test_2";
+
+            [[s1 should] equal:s2];
+            [[s2 shouldNot] equal:s3];
+            [[s3 should] startWithString:s1];
+        });
+
+        it(@"Test stub", ^{
+
+            NSString* s1 = @"123";
+            [[theValue(s1.length) should] equal:theValue(3)];
+
+            // Since now length will be equal 5;
+            [s1 stub:@selector(length) andReturn:theValue(5)];
+
+            [[theValue(s1.length) shouldNot] equal:theValue(3)];
+            [[theValue(s1.length) should] equal:theValue(5)];
+        });
+
+        it(@"Test mock", ^{
+
+            NSString* s1 = [NSString nullMock];
+
+            [[theValue(s1.length) should] equal:theValue(0)];
+            [[s1 substringFromIndex:0] shouldBeNil];
+
+            [s1 stub:@selector(substringFromIndex:) andReturn:@"substring"];
+
+            [[s1 substringFromIndex:0] shouldNotBeNil];
+            [[[s1 substringFromIndex:0] should] equal:@"substring"];
+        });
+
+    });
+
+});
+
+SPEC_END
+
--- Podfile
+++ Podfile
@@ -3,8 +3,7 @@ platform :ios, '6.0'
 pod 'AFNetworking'
 
 target :MyHoardTests, :exclusive => true do
-    pod 'Kiwi'
-    pod 'OCMock'
+    pod 'Kiwi/XCTest'
 end
 
 
