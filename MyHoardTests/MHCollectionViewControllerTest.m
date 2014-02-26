f0af6c8bafd49f927606a2a97d3f038b0b8c6582
Mateusz Fidos <mateuszfidos@gmail.com>
2014-02-26 22:42:48 +0100

    ViewController test update

--- MyHoardTests/MHCollectionViewControllerTest.m
+++ MyHoardTests/MHCollectionViewControllerTest.m
@@ -10,6 +10,8 @@
 #import "CollectionViewController.h"
 #import "MHCollection.h"
 #import "MHDatabaseManager.h"
+#import "Kiwi.h"
+#import "MHCoreDataContextForTests.h"
 
 @interface MHCollectionViewControllerTest : XCTestCase
 
@@ -20,6 +22,13 @@
     UIStoryboard *storyboard;
     CollectionViewController *_vc;
     UITableViewCell *cell;
+}
+
+static id partialMockForView() {
+    
+    CollectionViewController *viewController = [[CollectionViewController alloc]init];
+    id mockViewController = [KWMock partialMockForObject:viewController];
+    return mockViewController;
 }
 
 - (void)setUp
@@ -94,3 +103,68 @@
 }
 
 @end
+
+SPEC_BEGIN(newTest)
+
+describe(@"MHCollectionViewController", ^{
+    
+    __block MHCoreDataContextForTests* mhc = nil;
+    
+    beforeEach(^{
+        mhc = [MHCoreDataContextForTests new];
+        [MHCoreDataContext stub:@selector(getInstance) andReturn:mhc];
+    });
+    
+    afterEach(^{
+        [mhc dropTestPersistentStore];
+        mhc = nil;
+    });
+    
+    it(@"Should return number of sections", ^{
+        
+        CollectionViewController *_vc = [[CollectionViewController alloc]init];
+        
+        id mockFetchedResultsController = [KWMock mockForClass:[NSFetchedResultsController class]];
+        [[mockFetchedResultsController stubAndReturn:[NSObject new]]section];
+        
+        id mockViewController = partialMockForView();
+        [[mockViewController stubAndReturn:mockFetchedResultsController]fetchedResultsController];
+        
+        NSInteger numberOfSections = [mockViewController numberOfSectionsInTableView:_vc.tableView];
+        
+        [[theValue(numberOfSections) should]equal:theValue(1)];
+        
+    });
+    
+    it(@"NSFetchResultsController should return object at index path", ^{
+        
+        [MHDatabaseManager insertCollectionWithObjId:@"testId" objName:@"name" objDescription:@"1" objTags:@[@"1", @"2"] objItemsNumber:nil objCreatedDate:[NSDate date] objModifiedDate:nil objOwner:nil];
+        
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"MHCollection" inManagedObjectContext:[MHCoreDataContext getInstance].managedObjectContext];
+        [fetchRequest setEntity:entity];
+        
+        NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"objName" ascending:NO];
+        
+        [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
+        
+        [fetchRequest setFetchBatchSize:20];
+        
+        NSFetchedResultsController *rc = [[NSFetchedResultsController alloc]initWithFetchRequest:fetchRequest managedObjectContext:mhc.managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
+        
+        NSError *error = nil;
+        
+        [rc performFetch:&error];
+        
+        MHCollection *collection = [rc objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+        
+        [[error should]beNil];
+        [[collection should]beNonNil];
+        [[collection.objName should]equal:@"name"];
+        
+        [MHDatabaseManager removeCollectionWithId:@"testId"];
+        
+    });
+});
+
+SPEC_END
