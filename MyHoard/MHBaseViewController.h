c4e6dc981794b35e93a6bbf2be6473139cadafa1
gpawlowicz <gpawlowicz@wi.zut.edu.pl>
2014-03-13 01:42:23 +0100

    Added MHBaseViewController class

---
+++ MyHoard/MHBaseViewController.h
@@ -0,0 +1,16 @@
+//
+//  MHBaseViewController.h
+//  MyHoard
+//
+//  Created by Grzegorz Pawłowicz on 12.03.2014.
+//  Copyright (c) 2014 BLStream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHBaseViewController : UIViewController
+
+@property (readwrite) BOOL MHHamburger;
+@property (readwrite) BOOL MHLogo;
+
+@end
