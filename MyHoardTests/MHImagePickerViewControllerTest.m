a88751add8240d5432b6b15b3c583180a6151946
KacperTlusty <kacper.tlusty@outlook.com>
2014-03-13 13:28:52 +0100

    Improved test to testLocationFromImage

--- MyHoardTests/MHImagePickerViewControllerTest.m
+++ MyHoardTests/MHImagePickerViewControllerTest.m
@@ -129,14 +129,8 @@
     
     CLLocationCoordinate2D testCoordinates = [_vc locationForImage:filePath];
 
-    NSString *testLatitude = [NSString stringWithFormat:@"%f", 53.4306916667];
-    NSString *latitudeFromImage = [NSString stringWithFormat:@"%f", testCoordinates.latitude];
-    
-    NSString *testLongtitude = [NSString stringWithFormat:@"%f", 14.5553416667];
-    NSString *longtitudeFromImage = [NSString stringWithFormat:@"%f", testCoordinates.longitude];
-
-    XCTAssertEqual([testLatitude doubleValue], [latitudeFromImage doubleValue], @"");
-    XCTAssertEqual([testLongtitude doubleValue], [longtitudeFromImage doubleValue], @"");
+    XCTAssertEqualWithAccuracy(testCoordinates.latitude, 53.43069166666667, 0.000000001);
+    XCTAssertEqualWithAccuracy(testCoordinates.longitude, 14.55534166666667, 0.000000001);
     
     
     
