eaaff6dff922c0e0b72e00bcbb5abc10fb2e9e4f
KacperTlusty <kacper.tlusty@outlook.com>
2014-03-20 14:33:22 +0100

    [Fixed] MHCollectionViewControllerTest

--- MyHoardTests/MHCollectionViewControllerTest.m
+++ MyHoardTests/MHCollectionViewControllerTest.m
@@ -2,32 +2,37 @@
 //  MHCollectionViewControllerTest.m
 //  MyHoard
 //
-//  Created by user on 2/24/14.
+//  Created by Kacper Tłusty on 15.03.2014.
 //  Copyright (c) 2014 BLStream. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 #import "MHCollectionViewController.h"
-#import "MHCollection.h"
-#import "MHDatabaseManager.h"
 #import "Kiwi.h"
+#import "MHDatabaseManager.h"
 #import "MHCoreDataContextForTests.h"
+#import "MHCollection.h"
 
 @interface MHCollectionViewControllerTest : XCTestCase
 
 @end
 
-@implementation MHCollectionViewControllerTest{
+@implementation MHCollectionViewControllerTest {
     
     UIStoryboard *storyboard;
     MHCollectionViewController *_vc;
+    MHCollectionCell *cell;
+    
 }
 
-static id partialMockForView() {
+
+static id partialMockForView()
+{
     
     MHCollectionViewController *viewController = [[MHCollectionViewController alloc]init];
     id mockViewController = [KWMock partialMockForObject:viewController];
     return mockViewController;
+    
 }
 
 - (void)setUp
@@ -36,6 +41,8 @@ static id partialMockForView() {
     storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     _vc = [storyboard instantiateViewControllerWithIdentifier:NSStringFromClass([MHCollectionViewController class])];
     [_vc view];
+    
+    
 }
 
 - (void)tearDown
@@ -45,18 +52,22 @@ static id partialMockForView() {
     [super tearDown];
 }
 
+
 - (void)testViewDidLoad {
     
     [_vc viewDidLoad];
     
     XCTAssertNotNil(_vc.fetchedResultsController, @"");
     
-    [MHDatabaseManager insertCollectionWithObjId:@"testId" objName:@"name" objDescription:@"1" objTags:@[@"1", @"2"] objItemsNumber:nil objCreatedDate:[NSDate date] objModifiedDate:nil objOwner:nil];
+    [MHDatabaseManager insertCollectionWithObjId:@"testId" objName:@"test" objDescription:@"1" objTags:@[@"1", @"2"] objItemsNumber:nil objCreatedDate:[NSDate date] objModifiedDate:nil objOwner:nil];
     
-    MHCollection *fetchedCollection = [_vc.fetchedResultsController objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
     
-    XCTAssertNotNil(fetchedCollection, @"");
-    XCTAssertEqualObjects(fetchedCollection.objId, @"testId", @"");
+    MHCollection *fetchedItem1 = [_vc.fetchedResultsController objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    
+    
+    XCTAssertNotNil(fetchedItem1, @"");
+    XCTAssertEqualObjects(fetchedItem1.objName, @"name2", @"");
+    
     
     [MHDatabaseManager removeCollectionWithId:@"testId"];
 }
@@ -71,58 +82,48 @@ static id partialMockForView() {
     XCTAssertNotNil(_vc, @"");
 }
 
-
 #pragma Outlets
 
-- (void)testThatNavigationItemTitleExist {
-    
-    XCTAssertNotNil(_vc.navigationItem.title, @"");
-}
-
-- (void)testThatAddButtonExist {
-    
-    XCTAssertNotNil(_vc.navigationItem.rightBarButtonItem, @"");
+- (void)testThatCollectionViewExist {
+    XCTAssertNotNil(_vc.collectionView, @"");
 }
 
 @end
 
-SPEC_BEGIN(newTest)
+SPEC_BEGIN(collectionViewControllerTest)
 
 describe(@"MHCollectionViewController", ^{
     
-    __block MHCoreDataContextForTests* mhc = nil;
+    __block MHCoreDataContextForTests* mhi = nil;
     
     beforeEach(^{
-        mhc = [MHCoreDataContextForTests new];
-        [MHCoreDataContext stub:@selector(getInstance) andReturn:mhc];
+        mhi = [MHCoreDataContextForTests new];
+        [MHCoreDataContext stub:@selector(getInstance) andReturn:mhi];
     });
     
     afterEach(^{
-        [mhc dropTestPersistentStore];
-        mhc = nil;
+        [mhi dropTestPersistentStore];
+        mhi = nil;
     });
-    
     it(@"Should return number of sections", ^{
-        
         id mockFetchedResultsController = [KWMock mockForClass:[NSFetchedResultsController class]];
-        [[mockFetchedResultsController stubAndReturn:@[[NSObject new]]]sections];
+        [[mockFetchedResultsController stubAndReturn:[NSObject new]]section];
         
         id mockViewController = partialMockForView();
         [[mockViewController stubAndReturn:mockFetchedResultsController]fetchedResultsController];
         
-        UITableView *tableView = [[UITableView alloc]init];
-        id mockTableView = [KWMock partialMockForObject:tableView];
-        [[mockTableView stubAndReturn:[KWValue valueWithInteger:1]]numberOfSectionsInTableView:mockTableView];
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        MHCollectionViewController *_vc = [storyboard instantiateViewControllerWithIdentifier:NSStringFromClass([MHCollectionViewController class])];
+        id mockCollectionViewController = [KWMock partialMockForObject:_vc.collectionView];
+        [[mockCollectionViewController stubAndReturn:[KWValue valueWithInteger:1]]numberOfSectionsInCollectionView:mockCollectionViewController];
         
-        NSInteger numberOfSections = [mockViewController numberOfSectionsInTableView:mockTableView];
+        NSInteger numberOfSections = [mockViewController numberOfSectionsInCollectionView:mockCollectionViewController];
         
-        [[theValue(numberOfSections) should]equal:theValue(1)];
-        
+        [[theValue(numberOfSections) should]equal:theValue(0)];
     });
-    
-    it(@"Should return number of rows in section", ^{
-       
-        NSInteger numberOfRows = 23;
+    it(@"Should return number of cells in section", ^{
+        
+        NSInteger numberOfRows = 10;
         
         id mockSectionInfo = [KWMock mockForProtocol:@protocol(NSFetchedResultsSectionInfo)];
         [[mockSectionInfo stubAndReturn:[KWValue valueWithInteger:numberOfRows]]numberOfObjects];
@@ -130,29 +131,32 @@ describe(@"MHCollectionViewController", ^{
         id mockFetchedResultsController = [KWMock mockForClass:[NSFetchedResultsController class]];
         [[mockFetchedResultsController stubAndReturn:@[mockSectionInfo]]sections];
         
-        UITableView *tableView = [[UITableView alloc]init];
-        id mockTableView = [KWMock partialMockForObject:tableView];
-        [[mockTableView stubAndReturn:[KWValue valueWithInteger:numberOfRows]]numberOfRowsInSection:0];
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        MHCollectionViewController *_vc = [storyboard instantiateViewControllerWithIdentifier:NSStringFromClass([MHCollectionViewController class])];
+        id mockCollectionViewController = [KWMock partialMockForObject:_vc.collectionView];
+        [[mockCollectionViewController stubAndReturn:[KWValue valueWithInteger:numberOfRows]] numberOfRowsInSection:0];
+        
         
         id mockViewController = partialMockForView();
         
         [[mockViewController stubAndReturn:mockFetchedResultsController] fetchedResultsController];
         
-        NSInteger numberOfRowsInSection = [mockTableView numberOfRowsInSection:0];
         NSInteger numberORISFromMSI = [mockSectionInfo numberOfObjects];
-        NSInteger numberOSIMFRC = [[mockFetchedResultsController sections] count];
-        NSInteger numberORISFromMVC = [mockViewController numberOfSectionsInTableView:mockTableView];
+        NSInteger numberOSIMFRC = [[mockCollectionViewController sections] count];
+        NSInteger numberOSFromMVC = [mockViewController numberOfSectionsInCollectionView:mockCollectionViewController];
         
-        [[theValue(numberOfRowsInSection)should]equal:theValue(numberOfRows)];
+        
+        
         [[theValue(numberOfRows)should]equal:theValue(numberORISFromMSI)];
-        [[theValue(numberOSIMFRC)should]equal:theValue(1)];
-        [[theValue(numberORISFromMVC)should]equal:theValue(1)];
+        [[theValue(numberOSIMFRC)should]equal:theValue(0)];
+        [[theValue(numberOSFromMVC)should]equal:theValue(0)];
         
     });
-    
     it(@"NSFetchResultsController should return object at index path", ^{
         
         [MHDatabaseManager insertCollectionWithObjId:@"testId" objName:@"name" objDescription:@"1" objTags:@[@"1", @"2"] objItemsNumber:nil objCreatedDate:[NSDate date] objModifiedDate:nil objOwner:nil];
+        
+        [MHDatabaseManager insertCollectionWithObjId:@"testId2" objName:@"name2" objDescription:@"1" objTags:@[@"1", @"2"] objItemsNumber:nil objCreatedDate:[NSDate date] objModifiedDate:nil objOwner:nil];
         
         NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
         NSEntityDescription *entity = [NSEntityDescription entityForName:@"MHCollection" inManagedObjectContext:[MHCoreDataContext getInstance].managedObjectContext];
@@ -164,21 +168,25 @@ describe(@"MHCollectionViewController", ^{
         
         [fetchRequest setFetchBatchSize:20];
         
-        NSFetchedResultsController *rc = [[NSFetchedResultsController alloc]initWithFetchRequest:fetchRequest managedObjectContext:mhc.managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
+        NSFetchedResultsController *rc = [[NSFetchedResultsController alloc]initWithFetchRequest:fetchRequest managedObjectContext:mhi.managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
         
         NSError *error = nil;
         
         [rc performFetch:&error];
         
-        MHCollection *collection = [rc objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
-        
+        MHCollection *collectionOne = [rc objectAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
+        MHCollection *collectionTwo = [rc objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
         [[error should]beNil];
-        [[collection should]beNonNil];
-        [[collection.objName should]equal:@"name"];
+        [[collectionOne should]beNonNil];
+        [[collectionOne.objName should]equal:@"name"];
+        [[collectionTwo should]beNonNil];
+        [[collectionTwo.objName should]equal:@"name2"];
         
         [MHDatabaseManager removeCollectionWithId:@"testId"];
+        [MHDatabaseManager removeCollectionWithId:@"testId2"];
         
     });
+    
 });
 
 SPEC_END
