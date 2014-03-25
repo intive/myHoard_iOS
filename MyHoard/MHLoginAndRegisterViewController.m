ea6b64e4afa5d29e5c4088d73397a56e4f27ae50
Mateusz Fidos <mateuszfidos@gmail.com>
2014-03-25 14:42:02 +0100

    MYHOARD-459 bug fix

--- MyHoard/MHLoginAndRegisterViewController.m
+++ MyHoard/MHLoginAndRegisterViewController.m
@@ -86,6 +86,16 @@
         
         _passwordTextField.delegate = self;
         _passwordTextField1.delegate = self;
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardDidShow:)
+                                                     name:UIKeyboardDidShowNotification
+                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardDidHide:)
+                                                     name:UIKeyboardDidHideNotification
+                                                   object:nil];
     }
 
 }
@@ -106,6 +116,25 @@
     
     [textField resignFirstResponder];
     return NO;
+}
+
+- (void)slideFrame:(BOOL)yesNo {
+    
+    int movement = (yesNo ? -50 : 50);
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    }];
+}
+
+- (void)keyboardDidShow: (NSNotification *) notif{
+    
+    [self slideFrame: YES];
+}
+
+- (void)keyboardDidHide: (NSNotification *) notif{
+    
+    [self slideFrame: NO];
 }
 
 /*
