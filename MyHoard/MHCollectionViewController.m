6b74af43fff1fc65a8dd9587a739a7873f7df4b4
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2014-03-31 21:36:35 +0200

    Changed collections sorting mode.

--- MyHoard/MHCollectionViewController.m
+++ MyHoard/MHCollectionViewController.m
@@ -377,7 +377,7 @@ newIndexPath:(NSIndexPath *)newIndexPath
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc]init];
     NSEntityDescription *entity = [NSEntityDescription entityForName:@"MHCollection" inManagedObjectContext:[MHCoreDataContext getInstance].managedObjectContext];
     [fetchRequest setEntity:entity];
-    NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"objCreatedDate" ascending:YES];
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"objCreatedDate" ascending:NO];
     [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
     [fetchRequest setFetchBatchSize:20];
     NSFetchedResultsController *frc = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:[MHCoreDataContext getInstance].managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
