c5581c051a519a1d05de87afe307f74c582171f0
Kacper Tlusty <kacper.tlusty@outlook.com>
2014-04-19 19:44:46 +0200

    MHCollectionDetailsViewController protocols

--- MyHoard/MHCollectionDetailsViewController.h
+++ MyHoard/MHCollectionDetailsViewController.h
@@ -19,7 +19,7 @@
 #import "UIActionSheet+ButtonState.h"
 
 
-@interface MHCollectionDetailsViewController : MHBaseViewController <UICollectionViewDataSource, UICollectionViewDelegate>
+@interface MHCollectionDetailsViewController : MHBaseViewController <UICollectionViewDataSource, UICollectionViewDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (nonatomic, strong) MHCollection *collection;
