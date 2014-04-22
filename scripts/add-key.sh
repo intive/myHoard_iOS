1d9a4addb4fb34c52c1accd4315c98a8641d038e
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2014-04-22 14:33:25 +0200

    set default keychain after it has been created.

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-
