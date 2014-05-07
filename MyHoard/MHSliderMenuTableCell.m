eddf5f91043ef2d5c50f69b32c1707fd04807fc6
Mateusz Fidos <mateuszfidos@gmail.com>
2014-05-07 22:38:00 +0200

    removed cellSeparatorView completely

--- MyHoard/MHSliderMenuTableCell.m
+++ MyHoard/MHSliderMenuTableCell.m
@@ -8,22 +8,7 @@
 
 #import "MHSliderMenuTableCell.h"
 
-@interface MHSliderMenuTableCell()
-
-@property (nonatomic, strong) UIView* cellSeparatorView;
-
-@end
-
 @implementation MHSliderMenuTableCell
-
-- (UIView *) cellSeparatorView {
-    
-    if (!_cellSeparatorView) {
-        _cellSeparatorView = [[UIView alloc] initWithFrame:CGRectMake(12, self.bounds.size.height - 1, self.bounds.size.width - 12, 0.5)];
-        _cellSeparatorView.backgroundColor = [UIColor blackColor];
-    }
-    return _cellSeparatorView;
-}
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
 }
