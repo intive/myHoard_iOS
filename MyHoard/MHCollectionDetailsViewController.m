7d9002c96e2ae6fad79fd9e5cee5758ee2193474
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2014-05-20 11:54:32 +0200

    Cleaunp

--- MyHoard/MHCollectionDetailsViewController.m
+++ MyHoard/MHCollectionDetailsViewController.m
@@ -37,20 +37,6 @@ typedef NS_ENUM(NSInteger, ItemSortMode) {
     UIButton* _nameButton;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)awakeFromNib
-{
-    [super awakeFromNib];
-}
-
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
