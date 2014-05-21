b998e20f2de38cc0fd8e4c74ab94ec9f31925f7c
KacperTlusty <kacper.tlusty@outlook.com>
2014-05-21 23:00:46 +0200

    MYHOARD-792 - Fixed

--- MyHoard/MHAddCollectionViewController.m
+++ MyHoard/MHAddCollectionViewController.m
@@ -278,6 +278,20 @@ const NSInteger kAlertViewOne = 1;
         
         if (_collection) {
             
+            NSArray *collections = [[NSArray alloc] initWithArray:[MHDatabaseManager allCollections]];
+            for (MHCollection *col in collections) {
+                if([col.objName isEqualToString:self.nameTextField.text]) {
+                    if ([col.objCreatedDate isEqualToDate:_collection.objCreatedDate]) {
+                    } else {
+                        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"error" message:@"Collection of that name exists." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+                        [alert show];
+                    }
+                }
+                
+            }
+            
+
+            
             _collection.objName = trimmedString;
             _collection.objDescription = self.descriptionTextField.text;
             _collection.objModifiedDate = [NSDate date];
